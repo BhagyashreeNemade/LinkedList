9ec4f1045d82e78655fd8c054339b46f23834c4f
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-14 14:50:47 +0530

    create a simple linked list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("LinkedList");
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+
+            foreach (var ele in list)
+            {
+                Console.WriteLine(ele);
+            }
         }
     }
 }
